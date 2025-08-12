ede81e48a43d36a8804253d3dedfd865d098a278
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-08-12 21:36:23 +0530

    fix: fix issues of routers

--- app/routers/customer.py
+++ app/routers/customer.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from ..schemas import CustomerSchema
-from ..controllers.customer_controller import list_customers
+from ..controllers.customer_controller import get_all_customers
 from ..db import get_db
 
 router = APIRouter(prefix="/customers", tags=["customers"])
 
 @router.get("/", response_model=List[CustomerSchema])
 def read_customers(db: Session = Depends(get_db)):
-    return list_customers(db)
+    return get_all_customers(db)
