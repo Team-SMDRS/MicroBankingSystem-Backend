ec516874dfa54e2506271293f66967c626f360c9
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-09-02 21:42:41 +0530

    chore: make full backend suitable for text sql (remove ORM part )

--- app/controllers/customer_controller.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.orm import Session
-from ..services.customer_service import get_all_customers
-
-def list_customers(db: Session):
-    return get_all_customers(db)
---
+++ app/schemas/user_schema.py
@@ -0,0 +1,1 @@
+# manage users (view, create)
