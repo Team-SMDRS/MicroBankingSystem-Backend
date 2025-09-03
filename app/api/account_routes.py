81051dab7c2e505e52869375b75822ed9c0056bf
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-09-04 01:59:53 +0530

    feat:added JWT token based login

--- app/api/account_routes.py
+++ app/api/account_routes.py
@@ -1,1 +1,10 @@
  # manage accounts (create/view)
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/my_profile")
+def my_profile():
+    return {"message": "My Profile"}
--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Request
 from app.api import auth_routes
 from app.middleware.auth_middleware import AuthMiddleware
-from fastapi.responses import RedirectResponse
+from app.api import account_routes
+from fastapi.responses import JSONResponse
 
+#from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 # Middleware
+
 app.add_middleware(AuthMiddleware)
 
 
 
 # Routes
 app.include_router(auth_routes.router)
-
-
-
-
-
+app.include_router(account_routes.router)
+app.include_router(account_routes.router)
 
 
 
@@ -25,3 +25,19 @@ app.include_router(auth_routes.router)
 @app.get("/")
 async def root():
     return "Hello, Team SMTDS !"
+
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # change to your allowed origins
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+@app.exception_handler(404)
+async def not_found_handler(request: Request, exc):
+    return JSONResponse(
+        status_code=404,
+        content={"error": "Route not found. Please check the URL."}
+    )
